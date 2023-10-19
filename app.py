f622777d433ade33818c62589b144c0c23935e14
kn1ghtmare03 <103210796+kn1ghtmare03@users.noreply.github.com>
2023-10-19 17:32:21 +0000

    added jobhtml for dynamic data rendering

--- app.py
+++ app.py
@@ -2,9 +2,40 @@ from flask import Flask , render_template
 
 app = Flask(__name__)
 
+JOBS= [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'company':'Google',
+    'location':'Mumbai, India',
+    'salary':'$50,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist ',
+    'company':'Google',
+    'location':'Delhi, India',
+    'salary':'$80,000'
+  },
+  {
+  'id':3,
+  'title':'Data Analyst',
+  'company':'Google',
+  'location':'Bengaluru, India',
+  'salary':'$70,000'
+},
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'company':'Microsoft',
+    'location':'Mumbai, India',
+    'salary':'$150,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
 
 
 if __name__ == "__main__":
