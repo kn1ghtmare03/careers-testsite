4b2e386a042169b7fc1e9f172ec5b38804a445e0
kn1ghtmare03 <103210796+kn1ghtmare03@users.noreply.github.com>
2023-10-19 17:40:41 +0000

    added api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
@@ -36,6 +36,9 @@ JOBS= [
 @app.route("/")
 def hello_world():
   return render_template('home.html',jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
